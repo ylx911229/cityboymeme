05dc5e9817c1de47ec10d1afd5694b977255dff9
yaoluxing <yaoluxing@bytedance.com>
2026-01-17 15:34:38 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-8 mx-auto bg-white dark:bg-black sm:items-start sm:py-32 sm:px-16">
         <Image
           className="dark:invert"
           src="/next.svg"
